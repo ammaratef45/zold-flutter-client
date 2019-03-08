0bdde8b563318dcfc383baf2cc651d1bbd4bb975
Ammar Atef <a_atef45@yahoo.com>
2019-03-08 23:26:51 +0200

    #19 added for android

--- zold_wallet/lib/main.dart
+++ zold_wallet/lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import './home_page/home_page.dart';
 import './login_page/login_page.dart';
+import 'package:flutter_crashlytics/flutter_crashlytics.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() async {
+  bool isInDebugMode = false;
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isInDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  await FlutterCrashlytics().initialize();
+
+  runZoned<Future<Null>>(() async {
+    runApp(MyApp());
+  }, onError: (error, stackTrace) async {
+    await FlutterCrashlytics().reportCrash(error, stackTrace, forceCrash: false);
+  });
+}
 
 class MyApp extends StatelessWidget {
   final Widget myHome = LoginPage();
