05ab22ebe9f7ec42660ee741609c5ab3155a9299
Ammar Atef <a_atef45@yahoo.com>
2019-05-17 11:09:10 +0200

    solved transaction view lint errors

--- zold_wallet/lib/stateless_views/transaction_view.dart
+++ zold_wallet/lib/stateless_views/transaction_view.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:zold_wallet/transaction.dart';
 
+/// transaction record.
 class TransactionView extends StatelessWidget {
-  final Transaction transaction;
-  TransactionView(
+  /// constructor
+  const TransactionView(
     this.transaction
   );
+  /// model of the view
+  final Transaction transaction;
 
   @override
-  Widget build(BuildContext context) {
-    Widget widget = Container(
-      margin: EdgeInsets.only(top: 10, left: 2, right: 2),
+  Widget build(BuildContext context) =>
+    Container(
+      margin: const EdgeInsets.only(top: 10, left: 2, right: 2),
       color: Color(transaction.zents>0?0xFF00FF00:0xFFFF0000),
       child: Column(
         children: <Widget>[
@@ -19,11 +22,11 @@ class TransactionView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.only(right: 8),
-                child: Text('#' + transaction.id.toString()),
+                padding: const EdgeInsets.only(right: 8),
+                child: Text('#${transaction.id.toString()}'),
               ),
               Padding(
-                padding: EdgeInsets.only(right: 8),
+                padding: const EdgeInsets.only(right: 8),
                 child: Text(transaction.amount()),
               ),
               Text(transaction.timeAgo())
@@ -34,14 +37,12 @@ class TransactionView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Flexible(
-                child: Text(transaction.sender + ": " + transaction.details),
+                child: Text('${transaction.sender}: ${transaction.details}'),
               ),
             ],
           ),
         ],
       ),
     );
-    return widget;
-  }
-  
+
 }
