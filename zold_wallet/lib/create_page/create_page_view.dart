72113f39ac6d8f5a73c58a34c4090cfdd893d6dc
amorenew <amorenew>
2019-05-24 07:01:00 +0200

    Add title for Create Invoice & Send ZLD

--- zold_wallet/lib/create_page/create_page_view.dart
+++ zold_wallet/lib/create_page/create_page_view.dart
@@ -7,95 +7,95 @@ import 'package:zold_wallet/stateless_views/text_field.dart';
 
 /// view of invoice page
 class CreatePageView extends CreatePageViewModel {
-  Widget _copy() =>
-    Container(
-      child: FloatingActionButton(
-        onPressed: copyContent,
-        tooltip: 'Copy',
-        heroTag: 'Copy',
-        child: const Icon(Icons.content_copy),
-      ),
-    );
+  Widget _copy() => Container(
+        child: FloatingActionButton(
+          onPressed: copyContent,
+          tooltip: 'Copy',
+          heroTag: 'Copy',
+          child: const Icon(Icons.content_copy),
+        ),
+      );
 
-  Widget _share() =>
-    Container(
-      child: FloatingActionButton(
-        onPressed: captureAndSharePng,
-        tooltip: 'Share',
-        heroTag: 'Share',
-        child: const Icon(Icons.share),
-      ),
-    );
+  Widget _share() => Container(
+        child: FloatingActionButton(
+          onPressed: captureAndSharePng,
+          tooltip: 'Share',
+          heroTag: 'Share',
+          child: const Icon(Icons.share),
+        ),
+      );
 
   @override
-  Widget build(BuildContext context) =>
-    Scaffold(
-      key: snackKey,
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        shrinkWrap: true,
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          switch(index) {
-            case 0:
-              return SafeArea(
+  Widget build(BuildContext context) => Scaffold(
+        key: snackKey,
+        body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          shrinkWrap: true,
+          itemCount: 1,
+          itemBuilder: (BuildContext context, int index) {
+            switch (index) {
+              case 0:
+                return SafeArea(
                   child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ZoldTextField(
-                      controller: amountController,
-                      width: 150,
-                      hint: 'Amount: eg. 1.2',
-                    ),
-                    ZoldTextField(
-                      controller: messageController,
-                      width: 310,
-                      hint: 'Details: eg. for selling me the book',
-                    ),
-                    RaisedButton(
-                      child: const Text('Create'),
-                      onPressed: createQR,
-                    ),
-                    Visibility(
-                      visible: created,
-                      child: RepaintBoundary(
-                        key: globalKey,
-                        child: QrImage(
-                          version: 6,
-                          data: qrString,
-                          onError: (dynamic ex) {
-                            Dialogs.messageDialog(context, '[QR] ERROR',
-                             ex.toString(), snackKey);
-                          },
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Text('Create Invoice'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ZoldTextField(
+                        controller: amountController,
+                        width: 150,
+                        hint: 'Amount: eg. 1.2',
+                      ),
+                      ZoldTextField(
+                        controller: messageController,
+                        width: 310,
+                        hint: 'Details: eg. for selling me the book',
+                      ),
+                      RaisedButton(
+                        child: const Text('Create'),
+                        onPressed: createQR,
+                      ),
+                      Visibility(
+                        visible: created,
+                        child: RepaintBoundary(
+                          key: globalKey,
+                          child: QrImage(
+                            version: 6,
+                            data: qrString,
+                            onError: (dynamic ex) {
+                              Dialogs.messageDialog(context, '[QR] ERROR',
+                                  ex.toString(), snackKey);
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    Visibility(
-                      visible: created,
-                      child: Text(qrString),
-                    ),
-                    InkWell(
-                      child: Text(
-                        'Go back to home screen',
-                        style: TextStyle(color: Colors.blue),
+                      Visibility(
+                        visible: created,
+                        child: Text(qrString),
                       ),
-                      onTap: ()=>Navigator.pop(context),
-                    ),
-                  ],
-                ),
-              );
-          }
-        },
-      ),
-      floatingActionButton: AnimatedFloatingActionButton(
-        fabButtons: <Widget>[
-          _copy(),
-          _share(),
-        ],
-        colorStartAnimation: Colors.blue,
-        colorEndAnimation: Colors.red,
-        animatedIconData: AnimatedIcons.menu_close,
-      ),
-    );
-
+                      InkWell(
+                        child: Text(
+                          'Go back to home screen',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        onTap: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                );
+            }
+          },
+        ),
+        floatingActionButton: AnimatedFloatingActionButton(
+          fabButtons: <Widget>[
+            _copy(),
+            _share(),
+          ],
+          colorStartAnimation: Colors.blue,
+          colorEndAnimation: Colors.red,
+          animatedIconData: AnimatedIcons.menu_close,
+        ),
+      );
 }
--- zold_wallet/lib/stateless_views/pay_view.dart
+++ zold_wallet/lib/stateless_views/pay_view.dart
@@ -3,93 +3,92 @@ import 'package:zold_wallet/stateless_views/text_field.dart';
 import 'package:zold_wallet/wallet.dart';
 
 typedef PayCallback = void Function(
-  String bnf, String amount, String details, String keygap
-);
+    String bnf, String amount, String details, String keygap);
 
 /// payview that prompt for pay details
 class PayView extends StatelessWidget {
   /// constructor
-  const PayView({
-    this.payCallback,
-    this.authCallback,
-    this.keyGapAvailable,
-    this.controllers
-  });
+  const PayView(
+      {this.payCallback,
+      this.authCallback,
+      this.keyGapAvailable,
+      this.controllers});
 
   /// controllers of text editors
   final Map<String, TextEditingController> controllers;
+
   /// callback when pressing pay
   final PayCallback payCallback;
+
   /// callback when fingerprint is clicked
   final VoidCallback authCallback;
+
   /// determine if keygap is available in shared preferences
   final bool keyGapAvailable;
 
   @override
-  Widget build(BuildContext context) =>
-    Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ZoldTextField(
-            controller: controllers['bnfController'],
-            width: 210,
-            hint: 'bnf: eg. ammaratef45',
-          ),
-          ZoldTextField(
-            controller: controllers['amountController'],
-            width: 150,
-            hint: 'Amount: eg. 1.2',
-          ),
-          Text(
-            'Balance: ${Wallet.instance().balance()}',
-            style: TextStyle(
-              color: Colors.red
+  Widget build(BuildContext context) => Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('Send ZLD'),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              ZoldTextField(
-                controller: controllers['keygapController'],
-                width: 110,
-                hint: 'keygap',
+            ZoldTextField(
+              controller: controllers['bnfController'],
+              width: 210,
+              hint: 'bnf: eg. ammaratef45',
+            ),
+            ZoldTextField(
+              controller: controllers['amountController'],
+              width: 150,
+              hint: 'Amount: eg. 1.2',
+            ),
+            Text(
+              'Balance: ${Wallet.instance().balance()}',
+              style: TextStyle(color: Colors.red),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                ZoldTextField(
+                  controller: controllers['keygapController'],
+                  width: 110,
+                  hint: 'keygap',
+                ),
+                Visibility(
+                    visible: keyGapAvailable,
+                    child: IconButton(
+                      icon: const Icon(Icons.fingerprint),
+                      onPressed: authCallback,
+                    ))
+              ],
+            ),
+            ZoldTextField(
+              controller: controllers['messageController'],
+              width: 310,
+              hint: 'Details: eg. for selling me the book',
+            ),
+            RaisedButton(
+              onPressed: () {
+                payCallback(
+                    controllers['bnfController'].text,
+                    controllers['amountController'].text,
+                    controllers['messageController'].text,
+                    controllers['keygapController'].text);
+              },
+              child: const Text('Send'),
+            ),
+            InkWell(
+              child: Text(
+                'Go back to home screen',
+                style: TextStyle(color: Colors.blue),
               ),
-              Visibility(
-                visible: keyGapAvailable,
-                child: IconButton(
-                  icon: const Icon(Icons.fingerprint),
-                  onPressed: authCallback,
-                )
-              )
-            ],
-          ),
-          ZoldTextField(
-            controller: controllers['messageController'],
-            width: 310,
-            hint: 'Details: eg. for selling me the book',
-          ),
-          RaisedButton(
-            onPressed: (){
-              payCallback(
-                controllers['bnfController'].text,
-                controllers['amountController'].text,
-                controllers['messageController'].text,
-                controllers['keygapController'].text
-              );
-            },
-            child: const Text('Send'),
-          ),
-          InkWell(
-            child: Text(
-              'Go back to home screen',
-              style: TextStyle(color: Colors.blue),
+              onTap: () => Navigator.pop(context),
             ),
-            onTap: ()=>Navigator.pop(context),
-          ),
-        ],
-      ),
-    );
-    
+          ],
+        ),
+      );
 }
