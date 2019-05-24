d9e35e179204521c057c7aa7c43ab3aa4d85767b
amorenew <amorenew>
2019-05-24 06:39:59 +0200

    adding default button theme to the app

--- zold_wallet/lib/main.dart
+++ zold_wallet/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zold_wallet/create_page/create_page.dart';
 import 'package:zold_wallet/home_page/home_page.dart';
@@ -5,13 +6,14 @@ import 'package:zold_wallet/log_page/log_page.dart';
 import 'package:zold_wallet/login_page/login_page.dart';
 import 'package:zold_wallet/pay_page/pay_page.dart';
 import 'package:trust_fall/trust_fall.dart';
+import 'package:flutter/foundation.dart';
 
 import 'stateless_views/untrusted_page.dart';
 
 Future<void> main() async {
-  if(await TrustFall.isTrustFall) {
+  if (await TrustFall.isTrustFall && kReleaseMode) {
     runApp(MaterialApp(
-       home: UntrustedPage(),
+      home: UntrustedPage(),
     ));
   } else {
     runApp(MyApp());
@@ -22,26 +24,27 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   final Widget _myHome = LoginPage();
   @override
-  Widget build(BuildContext context) =>
-    MaterialApp(
-      title: 'Zold Wallet',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'RobotoMono',
-          appBarTheme: AppBarTheme(
-              textTheme: TextTheme(title: TextStyle(fontSize: 18))),
-          textTheme: TextTheme(
-            body1: TextStyle(fontSize: 18),
-          )),
-      home: _myHome,
-      onGenerateRoute: _getRoute,
-      routes: <String, WidgetBuilder>{
-        '/login': (BuildContext context) => LoginPage(),
-        '/home': (BuildContext context) => HomePage(),
-        '/pay': (BuildContext context) => PayPage(),
-        '/create': (BuildContext context) => CreatePage(),
-      },
-    );
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Zold Wallet',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            fontFamily: 'RobotoMono',
+            appBarTheme: AppBarTheme(
+                textTheme: TextTheme(title: TextStyle(fontSize: 18))),
+            buttonTheme: ButtonThemeData(
+              buttonColor: Theme.of(context).accentColor,
+              textTheme: ButtonTextTheme.primary,
+            ),
+            textTheme: TextTheme(body1: TextStyle(fontSize: 18))),
+        home: _myHome,
+        onGenerateRoute: _getRoute,
+        routes: <String, WidgetBuilder>{
+          '/login': (BuildContext context) => LoginPage(),
+          '/home': (BuildContext context) => HomePage(),
+          '/pay': (BuildContext context) => PayPage(),
+          '/create': (BuildContext context) => CreatePage(),
+        },
+      );
 
   Route<dynamic> _getRoute(RouteSettings settings) {
     if (settings.name == '/log') {
@@ -50,11 +53,10 @@ class MyApp extends StatelessWidget {
     return null;
   }
 
-  MaterialPageRoute<dynamic>
-    _buildRoute(RouteSettings settings, Widget builder) =>
-    MaterialPageRoute<dynamic>(
-      settings: settings,
-      builder: (BuildContext ctx) => builder,
-    );
-
+  MaterialPageRoute<dynamic> _buildRoute(
+          RouteSettings settings, Widget builder) =>
+      MaterialPageRoute<dynamic>(
+        settings: settings,
+        builder: (BuildContext ctx) => builder,
+      );
 }
--- zold_wallet/lib/stateless_views/phone_view.dart
+++ zold_wallet/lib/stateless_views/phone_view.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:zold_wallet/stateless_views/text_field.dart';
+
 typedef StringCallback = void Function(String);
-/// PhoneView is the view that propmt for the phone number. 
+
+/// PhoneView is the view that propmt for the phone number.
 class PhoneView extends StatelessWidget {
   /// constructor
-  PhoneView(
-    {
-      this.onSendCode,
-      this.authCallback
-    }
-  );
+  PhoneView({this.onSendCode, this.authCallback});
 
   /// onSendCode callback will execute when send code being clicked.
   final StringCallback onSendCode;
+
   /// authCallback will execute when pressing login with auth.
   final VoidCallback authCallback;
 
   final TextEditingController _phoneController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) =>
-    Container(
+  Widget build(BuildContext context) => Container(
       padding: const EdgeInsets.all(8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,10 +33,8 @@ class PhoneView extends StatelessWidget {
           const Padding(
             padding: EdgeInsets.only(top: 18),
           ),
-          const Text(
-            'Enter your mobile phone number (digits only) and'
-            ' we will send you a secret code in a few seconds:'
-          ),
+          const Text('Enter your mobile phone number (digits only) and'
+              ' we will send you a secret code in a few seconds:'),
           const Padding(
             padding: EdgeInsets.only(top: 18),
           ),
@@ -48,9 +43,7 @@ class PhoneView extends StatelessWidget {
               const Padding(
                 padding: EdgeInsets.only(left: 4),
               ),
-              const Text(
-                '+'
-              ),
+              const Text('+'),
               ZoldTextField(
                 controller: _phoneController,
                 hint: 'Digits only...',
@@ -61,10 +54,9 @@ class PhoneView extends StatelessWidget {
             padding: EdgeInsets.only(top: 18),
           ),
           RaisedButton(
-            onPressed: (){
+            onPressed: () {
               onSendCode(_phoneController.text);
             },
-            color: Colors.blue,
             child: const Text('Send me the code'),
           ),
           const Padding(
@@ -78,7 +70,5 @@ class PhoneView extends StatelessWidget {
             onTap: authCallback,
           )
         ],
-      )
-    );
-
+      ));
 }
