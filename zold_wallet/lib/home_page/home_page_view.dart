9e499b3ae3a40908c78f8c86f85b899388361d5d
Ammar Atef <a_atef45@yahoo.com>
2019-06-17 09:56:40 +0200

    #104 added transactions count to info

--- zold_wallet/lib/home_page/home_page_view.dart
+++ zold_wallet/lib/home_page/home_page_view.dart
@@ -5,98 +5,90 @@ import 'package:zold_wallet/stateless_views/transaction_view.dart';
 import 'package:flutter/material.dart';
 import 'package:zold_wallet/wallet.dart';
 
-
 /// view of home page
 class HomePageView extends HomePageViewModel {
-
   @override
-  Widget build(BuildContext context) =>
-    Scaffold(
-      key: snackKey,
-      body: RefreshIndicator(
-        onRefresh: onRefresh,
-        child: SafeArea(
+  Widget build(BuildContext context) => Scaffold(
+        key: snackKey,
+        body: RefreshIndicator(
+          onRefresh: onRefresh,
+          child: SafeArea(
             child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: 4,
-            itemBuilder: (BuildContext context, int index) {
-              switch(index) {
-                case 0:
-                  return Container(
-                    padding: const EdgeInsets.only(left: 6, top: 6),
-                    alignment: const Alignment(-1, 0),
-                    child: Image.asset(
-                      'assets/icon/icon.png',
-                      fit: BoxFit.contain,
-                      height: 64,
-                    ),
-                  );
-                case 1:
-                  return Container(
-                    padding: const EdgeInsets.only(left: 6, top: 6),
-                    child: InformationView(
-                      idText: Wallet.instance().id,
-                      balanceText: Wallet.instance().balance(),
-                      balanceZents: Wallet.instance().zents(),
-                      balanceUSD: Wallet.instance().usd(),
-                      copyCallback: copyid
-                    )
-                  );
-                case 2:
-                  return ListView.builder(
-                    padding: const EdgeInsets.only(left: 6, top: 3),
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemCount: Wallet.instance().transactions.length,
-                    itemBuilder: (BuildContext context, int index) =>
-                      TransactionView(Wallet.instance().transactions[index]),
-                  );
-                case 3:
-                  return Visibility(
-                    visible: Wallet.instance().transactions.isEmpty,
-                    child: Center(
-                      child: Text(message)
-                    ),
-                  );
-              }
-            },
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (BuildContext context, int index) {
+                switch (index) {
+                  case 0:
+                    return Container(
+                      padding: const EdgeInsets.only(left: 6, top: 6),
+                      alignment: const Alignment(-1, 0),
+                      child: Image.asset(
+                        'assets/icon/icon.png',
+                        fit: BoxFit.contain,
+                        height: 64,
+                      ),
+                    );
+                  case 1:
+                    return Container(
+                        padding: const EdgeInsets.only(left: 6, top: 6),
+                        child: InformationView(
+                          idText: Wallet.instance().id,
+                          balanceText: Wallet.instance().balance(),
+                          balanceZents: Wallet.instance().zents(),
+                          balanceUSD: Wallet.instance().usd(),
+                          copyCallback: copyid,
+                          transactions: Wallet.instance().transactions.length,
+                        ));
+                  case 2:
+                    return ListView.builder(
+                      padding: const EdgeInsets.only(left: 6, top: 3),
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: Wallet.instance().transactions.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          TransactionView(
+                              Wallet.instance().transactions[index]),
+                    );
+                  case 3:
+                    return Visibility(
+                      visible: Wallet.instance().transactions.isEmpty,
+                      child: Center(child: Text(message)),
+                    );
+                }
+              },
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 4,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/pay');
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.file_download),
-              onPressed: refresh,
-            ),
-            IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: restart
-            ),
-            IconButton(
-              icon: const Icon(Icons.receipt),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/create');
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.exit_to_app),
-              onPressed: logout,
-            ),
-          ],
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 4,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/pay');
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.file_download),
+                onPressed: refresh,
+              ),
+              IconButton(icon: const Icon(Icons.delete), onPressed: restart),
+              IconButton(
+                icon: const Icon(Icons.receipt),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/create');
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.exit_to_app),
+                onPressed: logout,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-
+      );
 }
--- zold_wallet/lib/stateless_views/information_view.dart
+++ zold_wallet/lib/stateless_views/information_view.dart
@@ -4,58 +4,72 @@ import 'package:flutter/material.dart';
 class InformationView extends StatelessWidget {
   /// constructor
   const InformationView(
-    {
-      this.idText='',
-      this.balanceText='',
-      this.balanceZents='',
-      this.balanceUSD='',
-      this.copyCallback
-    }
-  );
+      {this.idText = '',
+      this.balanceText = '',
+      this.balanceZents = '',
+      this.balanceUSD = '',
+      this.copyCallback,
+      this.transactions});
+
   /// The text represents the wallet id
   final String idText;
+
   /// The text represents the balance of the wallet
   final String balanceText;
+
   /// Balance in zents as a String
   final String balanceZents;
+
   /// Balance in USD as a String
   final String balanceUSD;
+
+  /// Transactions count
+  final num transactions;
+
   /// the callback when copy is clicked
   final VoidCallback copyCallback;
   @override
-  Widget build(BuildContext context) =>
-    Container(
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              const Text('ID: '),
-              Flexible(
-                child: Text(idText),
-              ),
-              GestureDetector(
-                onTap: copyCallback,
-                child: const Icon(Icons.content_copy,),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 18),
-            child: Row(
+  Widget build(BuildContext context) => Container(
+        child: Column(
+          children: <Widget>[
+            Row(
               children: <Widget>[
-                const Text('Balance: '),
+                const Text('ID: '),
+                Flexible(
+                  child: Text(idText),
+                ),
                 GestureDetector(
-                  onTap: () {
-                    Scaffold.of(context).showSnackBar(SnackBar
-                      (content: Text(balanceZents)));
-                  },
-                  child: Text('$balanceText ($balanceUSD)'),
+                  onTap: copyCallback,
+                  child: const Icon(
+                    Icons.content_copy,
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
-  
+            Container(
+              padding: const EdgeInsets.only(top: 18),
+              child: Row(
+                children: <Widget>[
+                  const Text('Balance: '),
+                  GestureDetector(
+                    onTap: () {
+                      Scaffold.of(context)
+                          .showSnackBar(SnackBar(content: Text(balanceZents)));
+                    },
+                    child: Text('$balanceText ($balanceUSD)'),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 18),
+              child: Row(
+                children: <Widget>[
+                  Text('Transactions: $transactions'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
 }
