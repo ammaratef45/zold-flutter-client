945ec86a6c635570aa1095a8227225a33a83aaa7
Ammar Atef <a_atef45@yahoo.com>
2019-07-10 10:52:02 +0200

    #125 redesigned boxes of transactions

--- zold_wallet/lib/stateless_views/transaction_view.dart
+++ zold_wallet/lib/stateless_views/transaction_view.dart
@@ -4,49 +4,60 @@ import 'package:zold_wallet/transaction.dart';
 /// transaction record.
 class TransactionView extends StatelessWidget {
   /// constructor
-  const TransactionView(
-    this.transaction
-  );
+  const TransactionView(this.transaction);
+
   /// model of the view
   final Transaction transaction;
 
   @override
-  Widget build(BuildContext context) =>
-    Container(
-      margin: const EdgeInsets.only(top: 10, left: 2, right: 2),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(right: 8),
-                child: Text('#${transaction.id.toString()}'),
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 8),
-                child: Text(
-                  transaction.amount(),
-                  style: TextStyle(
-                    color: Color(transaction.zents>0?0xFF00FF00:0xFFFF0000),
+  Widget build(BuildContext context) => Container(
+        margin: const EdgeInsets.only(top: 10, left: 2, right: 2),
+        padding: const EdgeInsets.only(top: 10, bottom: 10, left: 8),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    transaction.amount(),
+                    style: TextStyle(
+                      color: Color(
+                          transaction.zents > 0 ? 0xFF09BB2D : 0xFFD40000),
+                    ),
                   ),
                 ),
-              ),
-              Text(transaction.timeAgo())
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Flexible(
-                child: Text('${transaction.sender}: ${transaction.details}'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(child: Text(transaction.details)),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  child: Text(transaction.sender),
+                ),
+                Flexible(
+                  child: Text(transaction.timeAgo()),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
 }
